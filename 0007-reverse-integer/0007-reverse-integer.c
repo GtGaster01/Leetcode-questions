9829013bc729a3d849b3dd765ce386251b87db1c
Arda Şimşek <arda.ae01@gmail.com>
2026-03-17 16:40:12 +0300

    Time: 0 ms (100%), Space: 9.4 MB (16.85%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,12 @@
+
+
+int reverse(int x){ 
+    long long result=0;
+    int temp=x;
+    while(temp!=0){
+        result=(result*10)+(temp%10);
+        if(result>INT_MAX ||result<INT_MIN) return 0;
+        temp/=10;
+    }
+    return result;
+}
